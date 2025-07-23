d4ceb977ae1db0ecd33fc081afc61b8679d7f928
cloudopsdojo <cloudopsdojo@gmail.com>
2025-07-23 14:18:26 +0530

    added ruleset

---
+++ terraform/ec2/ec2.tf
@@ -0,0 +1,47 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+# Create an S3 bucket
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+output "s3_arn" {
+    value = aws_s3_bucket.example.arn
+}
+
+output "aws-ce2_arn" {
+    value = aws_instance.web.arn
+}
+
+output "aws_ec2_public_ip" {
+    value = aws_instance.web.public_ip
+}
